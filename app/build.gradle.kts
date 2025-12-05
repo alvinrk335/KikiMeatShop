32ce011ede5fca70eb38438f24f9bebdbdf9e7e2
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-12-05 22:56:57 +0700

    added bottom navigation view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "id.ac.binus.kikimeatshop"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "id.ac.binus.kikimeatshop"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/id/ac/binus/kikimeatshop/MainActivity.java
+++ app/src/main/java/id/ac/binus/kikimeatshop/MainActivity.java
@@ -1,14 +1,26 @@
 package id.ac.binus.kikimeatshop;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setUpNavigationView();
+
     }
+
+    private void setUpNavigationView(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                boolean flag = false;
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        fragment = new HomeFragment();
+                        flag = true;
+                        break;
+                    case R.id.menu_products:
+                        fragment = new ProductFragment();
+                        flag = true;
+                        break;
+                }
+                if(flag){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                    return true;
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+
 }
